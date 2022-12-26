803b739c32a291ea48799b086e58904f77da4f0f
XtawVBotz <121201573+XtawVBotz@users.noreply.github.com>
2022-12-26 07:21:11 +0700

    Update owner.ts

--- src/commands/general/infobot/owner.ts
+++ src/commands/general/infobot/owner.ts
@@ -6,12 +6,12 @@ export default {
     category: 'bot-info',
     callback: async ({ msg, client, message }) => {
         const { from, sender } = msg
-        const vcard = 'BEGIN:VCARD\n' + 'VERSION:3.0\n' + 'FN:Pais\n' + 'ORG:Ashoka Uni;\n' + 'TEL;type=CELL;type=VOICE;waid=6285805609094:+62 858 0560 9094\n' + 'END:VCARD'
+        const vcard = 'BEGIN:VCARD\n' + 'VERSION:3.0\n' + 'FN:Pais\n' + 'ORG:Ashoka Uni;\n' + 'TEL;type=CELL;type=VOICE;waid=6285159907432:+62 851 5990 7432\n' + 'END:VCARD'
         return client.sendMessage(
             from,
             {
                 contacts: {
-                    displayName: 'Pais',
+                    displayName: 'Xtaw',
                     contacts: [{ vcard }],
                 },
                 mentions: [sender],
